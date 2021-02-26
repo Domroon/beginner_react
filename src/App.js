7d45a8f6484eb4c3f8c97fe0e7350b2f1ba51fc2
Dominik Haeusser <haeusser-dominik@web.de>
2021-02-26 12:19:16 +0100

    add props to Tweet

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import './App.css'
 
 function App(){
   return(
-    <div className="app">
-      <Tweet />
+    <div className="app" message="This is a random message">
+      <Tweet name="Dev ED" message="Whats up"/>
+      <Tweet name="John Snow" message="i am the true king"/>
+      <Tweet name="Traversy Media" message="Yesss 400k!"/>
+      <Tweet name="Mosh" message="Nochmal dabei sein nochmal!!"/>
     </div>
   );
 }
