be0147d7ee1a0d0b9fbb67cd23a5ee1978e5be18
Dominik Haeusser <haeusser-dominik@web.de>
2021-02-26 12:27:45 +0100

    add a counter with useState and a function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './sayHello';
 import Hello from './sayHello';
 import Tweet from './Tweet';
 import './App.css'
 
 function App(){
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+const increment = () => {
+  setCount(count + 1);
+}
+
   return(
-    <div className="app" message="This is a random message">
-      <Tweet name="Dev ED" message="Whats up"/>
-      <Tweet name="John Snow" message="i am the true king"/>
-      <Tweet name="Traversy Media" message="Yesss 400k!"/>
-      <Tweet name="Mosh" message="Nochmal dabei sein nochmal!!"/>
+    <div className="app">
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
