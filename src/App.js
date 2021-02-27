5a088ff2dd5bff87b77729cbb1ae851c7681ecc5
Dominik Haeusser <haeusser-dominik@web.de>
2021-02-27 22:12:50 +0100

    Add API style

--- src/App.js
+++ src/App.js
@@ -6,19 +6,17 @@ import './App.css'
 
 function App(){
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
+  const [users, setUser] = useState([
+    {name: 'Ed', message: 'Hello there'},
+    {name: 'John', message: 'I am John Snow'},
+    {name: 'traversy', message: 'Iam awesome'}
+  ]);
 
   return(
     <div className="app">
-      <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
